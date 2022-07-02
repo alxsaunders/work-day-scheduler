0e007b2dc545f73e2fd18e905eecf6e973cc359b
alxsaunders <alexsaunders242@gmail.com>
2022-07-01 20:57:30 -0400

    added classes to change color of time block

--- assets/script.js
+++ assets/script.js
@@ -7,10 +7,31 @@ let description = $(".description");
 let saveButton = $(".saveBtn");
 let currentHour = moment().hours()
 
+
 console.log(currentHour);
 console.log(typeof currentHour);
 
 description.each(function() {
-    let 
+    let timeBlock = parseInt($(this).attr('id'))
+
+    if(timeBlock === currentHour) {
+        $(this).addClass("present");
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+    
+    }
+    else if (timeBlock < currentHour) {
+        $(this).addClass("past")
+        $(this).removeClass("future")
+        $(this).removeClass("present")
+    }else {
+        $(this).addClass("future")
+        $(this).removeClass("past")
+        $(this).removeClass("present")
+    }
+
 })
+
+
+
 })
