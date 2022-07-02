1ecd49572929204936344f30d20c5f6297be7427
alxsaunders <alexsaunders242@gmail.com>
2022-07-01 20:28:54 -0400

    started script

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+$("#currentDay").text(moment().format("MMMM Do YYYY")); 
+
+
+let description = $(".description");
+let saveButton = $(".saveBtn");
+let currentHour = moment().hours()
+
+console.log(currentHour);
+console.log(typeof currentHour);
+
+description.each(function() {
+    let 
+})
+})
